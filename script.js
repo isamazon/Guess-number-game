c09c4c54eda792977b3135fe9d171b8635be494e
Isaiah Mazon <37008425+isamazon@users.noreply.github.com>
2020-11-13 16:16:43 -0800

    Adding score
    added score variable and added if statements that decrease the score everytime its not equal to the targetNum

--- script.js
+++ script.js
@@ -12,15 +12,27 @@ console.log(targetNum);
 // 2) Secondly we want to bring functionality to clicking on the 'Check' button to see if whatever number we put in equals the targetNum
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
+  let score = 20;
 
   if (guess === targetNum) {
-    document.querySelector('.message').textContent = 'You got it right!';
+    document.querySelector('.message').textContent = '🎉 Correct Number! 🎉';
     document.querySelector('.number').textContent = targetNum;
+    document.body.style.backgroundColor = 'green';
+    document.querySelector('.highscore').textContent = score;
   } else if (guess > targetNum) {
-    document.querySelector('.message').textContent = '📈 Too high!';
-  } else if (guess < targetNum && guess > 0) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game';
+      document.body.style.backgroundColor = 'Red';
+    }
+  } else if (guess < targetNum) {
     document.querySelector('.message').textContent = '📉 Too Low!';
-  } else {
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No number!';
   }
 });
